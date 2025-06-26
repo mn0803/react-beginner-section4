7ffa3c1ae75dd23e00becc4b9c792cc518f93e9e
mn0803 <watervalley83@gmail.com>
2025-06-26 15:18:51 +0900

    Section4

--- src/index.js
+++ src/index.js
@@ -22,20 +22,21 @@
 // letは再宣言不可能
 // let val2 = "let変数を再宣言";
 
-// const val3 = "const変数";
-// console.log(val3);
+//const val3 = "const変数";
+//console.log(val3);
 
 // // const変数は上書き不可
-// val3 = "const変数を上書き";
+//val3 = "const変数を上書き";
 
 // constは再宣言不可能
-// const val3 = "const変数を再宣言";
+//const val3 = "const変数を再宣言";
 
 // // constで定義したオブジェクトはプロパティの変更が可能
-// const val4 = {
-//   name: "じゃけぇ",
-//   age: 31
-// };
+//  const val4 = {
+//    name: "じゃけぇ",
+//    age: 31
+//  };
+//  console.log(val4);
 // val4.name = "jak";
 // val4.address = "Hiroshima";
 // console.log(val4);
@@ -44,21 +45,22 @@
 // const val5 = ["dog", "cat"];
 // val5[0] = "bird";
 // val5.push("monkey");
-// console.log(val5);
+// //monkeyという要素を追加
+//  console.log(val5);
 
 /**
  * テンプレート文字列 ``
  */
 // const name = "じゃけぇ";
 // const age = 31;
-// 「私の名前はじゃけぇです。年齢は31歳です。」
+// // 「私の名前はじゃけぇです。年齢は31歳です。」
 
-// // 従来の方法
-// const message1 = "私の名前は" + name + "です。年齢は" + age + "です。";
+// // // 従来の方法
+// const message1 = "私の名前は" + name + "です。年齢は" + age + "歳です。";
 // console.log(message1);
 
-// // テンプレート文字列を用いた方法
-// const message2 = `私の名前は${name}です。年齢は${age}です。`;
+// // // テンプレート文字列を用いた方法
+// const message2 = `私の名前は${name}です。年齢は${age}歳です。`;
 // console.log(message2);
 
 /**
@@ -66,29 +68,33 @@
  */
 // 従来の関数
 // function func1(str) {
-//   return str;
-// }
-// const func1 = function func1(str) {
-//   return str;
-// };
-// console.log(func1("func1です"));
+//    return str;
+//  }
+//  console.log(func1("func1です"));
+//  const func1 = function func1(str) {
+//    return str;
+//  };
+//  console.log(func1("func1です"));
 
 // アロー関数
 // const func2 = (str) => {
 //   return str;
 // };
 // console.log(func2("func2です"));
+// const func2 = str => str; 
+// console.log(func2("func2です"));
 
 // const func3 = (num1, num2) => {
 //   return num1 + num2;
 // };
+// const func3 =(num1, num2) => num1 + num2;
 
 // const func3 = (num1, num2) => ({
 //   hoge: num1,
 //   huga: num2,
 // });
 
-// console.log(func3(10, 20));
+//  console.log(func3(10, 20));
 
 /**
  * 分割代入
@@ -98,11 +104,12 @@
 //   age: 31
 // };
 
-// const message1 = `名前は${myProfile.name}です。年齢は${myProfile.age}です。`;
+// const message1 = `名前は${myProfile.name}です。年齢は${myProfile.age}歳です。`;
 // console.log(message1);
 
 // const { name, age } = myProfile;
-// const message2 = `名前は${name}です。年齢は${age}です。`;
+//myProfileからnameとageを取り出す
+// const message2 = `名前は${name}です。年齢は${age}歳です。`;
 // console.log(message2);
 
 // const myProfile = ["じゃけぇ", 31];
@@ -111,14 +118,16 @@
 // console.log(message3);
 
 // const [name, age] = myProfile;
+// // 分割代入するときに好きな変数名をつける
 // const message4 = `名前は${name}です。年齢は${age}です。`;
 // console.log(message4);
 
 /**
  * デフォルト値（引数、分割代入）
  */
-// const sayHello = (name = "ゲスト") => console.log(`こんちには!${name}さん!`);
+// const sayHello = (name = "ゲスト") => console.log(`こんにちは!${name}さん!`);
 // sayHello("じゃけぇ");
+// sayHello();
 
 // const myProfile = {
 //   age: 31
@@ -136,6 +145,8 @@
 //   name: name,
 //   age: age
 // };
+// console.log(myProfile);
+
 // const myProfile = {
 //   name,
 //   age
@@ -153,6 +164,7 @@
 // const sumFunc = (num1, num2) => console.log(num1 + num2);
 // sumFunc(arr1[0], arr1[1]);
 // sumFunc(...arr1);
+// 配列の中身を順番に処理する
 
 // まとめる
 // const arr2 = [1, 2, 3, 4, 5];
@@ -161,12 +173,13 @@
 // console.log(num2);
 // console.log(arr3);
 
-// 配列のコピー、結合
+// 配列のコピー、結合　スプレッド構文
 // const arr4 = [10, 20];
 // const arr5 = [30, 40];
 
 // const arr6 = [...arr4];
 // console.log(arr6);
+// arr4の名前は変わらない
 
 // const arr7 = [...arr4, ...arr5];
 // console.log(arr7);
@@ -174,9 +187,12 @@
 // const arr8 = arr4;
 // arr8[0] = 100;
 // console.log(arr8);
+// arr4の中身も変わる
 
 /**
  * mapやfilterを使った配列の処理
+ * map:配列を変換
+ * filter: 条件を満たすものだけ返す
  */
 // const nameArr = ['田中', '山田', 'じゃけぇ'];
 // for (let index = 0; index < nameArr.length; index++) {
@@ -218,8 +234,9 @@
 // const val1 = 1 > 0 ? "trueです" : "falseです";
 // console.log(val1);
 
-// const num = '1300';
+// const num = 1300;
 // console.log(num.toLocaleString());
+// 数値を三桁で区切る
 
 // const formattedNum = typeof num === 'number' ? num.toLocaleString() : '数値を入力してください';
 // console.log(formattedNum);
@@ -232,31 +249,31 @@
 /**
  * 論理演算子の本当の意味を知ろう && ||
  */
-// truthy、falsyについて
+// truthy、falsyについて　条件判定時に真偽として扱われる
 // "ABC" 0 10 undefined null false NaN "" [] {}
-const val = "ABC";
-if (val) {
-  console.log("valはtruthyです");
-} else {
-  console.log("valはfalsyです");
-}
+// const val = "ABC";
+// if (val) {
+//   console.log("valはtruthyです");
+// } else {
+//   console.log("valはfalsyです");
+// }
 
-const flag1 = true;
-const flag2 = false;
+// const flag1 = true;
+// const flag2 = true;
 
-if (flag1 || flag2) {
-  console.log("1か2はtrueになります");
-}
-if (flag1 && flag2) {
-  console.log("1も2もtrueになります");
-}
+// if (flag1 || flag2) {
+//   console.log("1か2はtrueになります");
+// }
+// if (flag1 && flag2) {
+//   console.log("1も2もtrueになります");
+// }
 
 // || は左側がtruthyの時その時点で返却する
-const num = null;
-const fee = num || "金額未設定";
-console.log(fee);
+// const num = null;
+// const fee = num || "金額未設定";
+// console.log(fee);
 
 // && は左側がfalsyの時その時点で返却する
-// const num2 = 100;
+// const num2 = null;
 // const fee2 = num2 && "何か設定されました";
 // console.log(fee2);
